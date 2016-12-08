76a1d83da36c334daea0455ca0b5d43fc1e6b68c
ballidaku <guffy2013>
2016-12-08 08:35:45 -0500

    New commit

---
+++ app/src/main/java/faris/idea/punch/MyUtilities/MySharedPreferences.java
@@ -0,0 +1,81 @@
+package faris.idea.punch.MyUtilities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by brst-pc93 on 11/24/16.
+ */
+
+public class MySharedPreferences
+{
+    private static MySharedPreferences instance = null;
+
+    public final String PreferenceName = "MyPreference";
+
+    public static MySharedPreferences getInstance()
+    {
+        if(instance == null)
+        {
+            instance=new MySharedPreferences();
+        }
+        return instance;
+    }
+
+
+    public SharedPreferences getPreference(Context context)
+    {
+        return context.getSharedPreferences(PreferenceName, Activity.MODE_PRIVATE);
+    }
+
+    public void saveCoins(Context context,int coinsCount)
+    {
+        SharedPreferences.Editor editor = getPreference(context).edit();
+        editor.putInt(MyConstants.COINS_COUNT, coinsCount);
+        editor.apply();
+    }
+
+
+    public int getCoins(Context context)
+    {
+       return getPreference(context).getInt(MyConstants.COINS_COUNT,0);
+    }
+
+    public boolean openInterstitialApp(Context context)
+    {
+       int count= getPreference(context).getInt(MyConstants.INTERSTITIAL_ADD_COUNT,0);
+
+        count++;
+
+        SharedPreferences.Editor editor = getPreference(context).edit();
+        editor.putInt(MyConstants.INTERSTITIAL_ADD_COUNT, count==3 ? 0 : count);
+        editor.apply();
+
+
+        if(count == 3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    public void buyWeapon(Context context,String weaponName)
+    {
+        SharedPreferences.Editor editor = getPreference(context).edit();
+        editor.putBoolean(weaponName, true);
+        editor.apply();
+    }
+
+    public boolean getWeaponStatus(Context context,String weaponName)
+    {
+        return getPreference(context).getBoolean(weaponName,false);
+    }
+
+
+
+}
---
+++ app/src/main/java/faris/idea/punch/MyUtilities/MyUtil.java
@@ -0,0 +1,70 @@
+package faris.idea.punch.MyUtilities;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+/**
+ * Created by brst-pc93 on 11/18/16.
+ */
+
+public class MyUtil
+{
+
+
+    public void showImageInGlide(Context context, ImageView imageView, int image)
+    {
+        try
+        {
+            Glide.with(context).load(image)
+                 //.crossFade()
+                 .asGif()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                 .into(imageView);
+        } catch (Exception e)
+        {
+
+            e.printStackTrace();
+            Glide.with(context)
+                 .load(image)
+                 .asGif()
+               .diskCacheStrategy(DiskCacheStrategy.ALL)
+                 .into(imageView);
+        }
+    }
+
+
+    public void setFont(Context context, TextView textView)
+    {
+        Typeface face = Typeface.createFromAsset(context.getAssets(), "LithosProBlack.ttf");
+
+        textView.setTypeface(face);
+
+    }
+
+    public void applyFont(final Context context, final View root)
+    {
+        try
+        {
+            if (root instanceof ViewGroup)
+            {
+                ViewGroup viewGroup = (ViewGroup) root;
+                for (int i = 0; i < viewGroup.getChildCount(); i++)
+                    applyFont(context, viewGroup.getChildAt(i));
+            }
+            else if (root instanceof TextView)
+                setFont(context, (TextView) root);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+}
